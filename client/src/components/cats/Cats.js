e83d140067d72b65416a9f3779c20ffb271cf8e9
Leonardo Pallone <leonardopallone@gmaill.com>
2022-03-28 20:40:50 -0600

    auth done

---
+++ client/src/components/cats/Cats.js
@@ -0,0 +1,7 @@
+const Cats = () => (
+  <>
+    <h1>My cats</h1>
+  </>
+)
+
+export default Cats;
--- db/migrate/20220329003852_add_trackable_to_users.rb
+++ db/migrate/20220329003852_add_trackable_to_users.rb
@@ -7,8 +7,6 @@ class AddTrackableToUsers < ActiveRecord::Migration[7.0]
         add_column :users, :current_sign_in_ip, :string
         add_column :users, :last_sign_in_ip, :string
       end
-    end
-
-
-  end
+  
+  
 end
